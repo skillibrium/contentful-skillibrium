a674868644fee0f1e202f15c54746b32d2045e1a
msubodh <munjal.subodh@gmail.com>
2023-01-13 08:08:23 -0500

    Ready to get methodologies

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,41 @@
 import "./style.css";
-import * as contentful from "contentful"
+import * as contentful from "contentful";
 
-const ACCESS_TOKEN = "7rjVXUpBnvUC4BXz5CK0udDwDZjauDREL4eSo98vuio";
-const SPACE = "sv54roagnofr";
+let client: contentful.ContentfulClientApi;
 
-const client = contentful.createClient({
-	accessToken: ACCESS_TOKEN,
-	space: SPACE,
-});
+test();
 
-client
-	.getEntry("E59dHv1djhlVMNf92ydOd")
-	.then((entry) => console.log(entry))
-	.catch((err) => console.log(err));
+/**
+ * Creates and connects a client that will be used for the rest of the session
+ * @param accessToken Access token to connect to Contentful
+ * @param space SpaceID for contentful
+ */
+function init(accessToken: string, space: string): void {
+	try {
+		client = contentful.createClient({
+			accessToken: accessToken,
+			space: space,
+		});
+	} catch (e) {
+		console.error(e);
+	}
+}
 
-function getClient (accessToken: string , space: string) {
-  try{
-    return contentful.createClient({
-					accessToken: accessToken,
-					space: space,
-				});
-  }
-  catch(e){
-    console.error(e)
-    
-  }
+// Methodology Functions
+
+function getMethodologies() {}
+
+/**
+ * Test function that will not be used in the actual codebase
+ */
+function test() {
+	const ACCESS_TOKEN = "7rjVXUpBnvUC4BXz5CK0udDwDZjauDREL4eSo98vuio";
+	const SPACE = "sv54roagnofr";
+
+	init(ACCESS_TOKEN, SPACE);
+
+	client
+		.getEntry("E59dHv1djhlVMNf92ydOd")
+		.then((entry) => console.log(entry))
+		.catch((err) => console.log(err));
 }
